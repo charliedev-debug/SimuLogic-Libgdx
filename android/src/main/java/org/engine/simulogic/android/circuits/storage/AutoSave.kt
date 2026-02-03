b30b7bbfbf917b5e4d92ccdbd13908cd1df85596
charliedev-debug <ngaricharlesdev@gmail.com>
2026-02-03 21:48:08 +0300

    added autosave

---
+++ android/src/main/java/org/engine/simulogic/android/circuits/storage/AutoSave.kt
@@ -0,0 +1,39 @@
+package org.engine.simulogic.android.circuits.storage
+
+import org.engine.simulogic.android.circuits.logic.Connection
+import org.engine.simulogic.android.utilities.Timer
+
+class AutoSave private constructor(private val projectOptions: ProjectOptions, private val connection: Connection) {
+
+   private val dto = DataTransferObject()
+    var enabled = true
+    companion object {
+        var dataChanged = false
+
+        lateinit var instance:AutoSave
+        fun initialize(projectOptions: ProjectOptions, connection: Connection){
+            instance = AutoSave(projectOptions, connection)
+        }
+    }
+
+    fun forceSave(){
+        if(dataChanged){
+            dto.writeData(projectOptions, connection)
+            dataChanged = false
+        }
+    }
+
+   // save the file at 1sec intervals
+   private val timer = Timer(10f, listener = object : Timer.ITimerListener{
+       override fun onTick() {
+           if(dataChanged && enabled) {
+               dto.writeData(projectOptions, connection)
+               dataChanged = false
+           }
+       }
+   })
+
+    fun run(){
+        timer.update()
+    }
+}
