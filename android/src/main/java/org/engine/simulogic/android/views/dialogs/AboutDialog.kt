f330e91be7732ccbd367d79c05006c54a69cc2b7
charliedev-debug <ngaricharlesdev@gmail.com>
2026-02-24 12:23:43 +0300

    added font size dropdown in label dialog

--- android/src/main/java/org/engine/simulogic/android/views/dialogs/AboutDialog.kt
+++ android/src/main/java/org/engine/simulogic/android/views/dialogs/AboutDialog.kt
@@ -5,16 +5,19 @@ import android.content.Context
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.ImageView
+import com.google.android.material.button.MaterialButton
 import org.engine.simulogic.R
 
 class AboutDialog (context: Context) : Dialog(context) {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view = layoutInflater.inflate(R.layout.about_project_dialog,null)
         view.findViewById<ImageView>(R.id.close).setOnClickListener {
             dismiss()
+        }
+        view.findViewById<MaterialButton>(R.id.review).setOnClickListener {
+
         }
         this.setContentView(view)
         this.setCancelable(true)
--- android/src/main/java/org/engine/simulogic/android/views/dialogs/LabelDialog.kt
+++ android/src/main/java/org/engine/simulogic/android/views/dialogs/LabelDialog.kt
@@ -3,29 +3,88 @@ package org.engine.simulogic.android.views.dialogs
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.text.InputType
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.ListPopupWindow
+import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textview.MaterialTextView
 import org.engine.simulogic.R
 import org.engine.simulogic.android.views.interfaces.IDialogLabelListener
 
 
 class LabelDialog(context: Context, private val listener:IDialogLabelListener) : Dialog(context) {
 
+
+    class CustomFontAdapter(context: Context,items:Array<String>):ArrayAdapter<String>(context, 0, items){
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+           val view =
+               convertView ?: LayoutInflater.from(context).inflate(R.layout.font_popup_list_item, parent, false)
+            val currentItem = getItem(position)
+            view.findViewById<MaterialTextView>(R.id.label).text = currentItem
+
+            return view
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view = layoutInflater.inflate(R.layout.dialog_label_name,null)
         val cancel = view.findViewById<AppCompatButton>(R.id.cancel)
         val save = view.findViewById<AppCompatButton>(R.id.save)
         val editTextFileName = view.findViewById<TextInputEditText>(R.id.fileName)
+        val fontDropDownEditText = view.findViewById<TextInputEditText>(R.id.fontDropDown)
+        val fontPopupButton = view.findViewById<AppCompatImageButton>(R.id.showPopup)
+        val fontSizeDefaults = arrayOf("SMALL - 25px", "MEDIUM - 45px", "LARGE - 60px", "LARGER - 75px", "CUSTOM")
+        val fontListPopupWindow = ListPopupWindow(context).apply { setBackgroundDrawable(ResourcesCompat.getDrawable(context.resources,R.drawable.popup_bg, null)) }
+        val fontAdapter =  CustomFontAdapter(context,fontSizeDefaults)//ArrayAdapter(context, android.R.layout.simple_dropdown_item_1line, fontSizeDefaults)
+        fontListPopupWindow.setAdapter(fontAdapter)
+        fontListPopupWindow.anchorView = fontDropDownEditText
+        fontDropDownEditText.isEnabled = false
+        fontDropDownEditText.setText("25")
+
+        fontListPopupWindow.setOnItemClickListener { parent, view, position, id ->
+            fontSizeDefaults[position].also { value->
+                fontDropDownEditText.isEnabled = false
+                when(value){
+                    "SMALL - 25px" ->{
+                        fontDropDownEditText.setText("25")
+                    }
+                    "MEDIUM - 45px" ->{
+                        fontDropDownEditText.setText("45")
+                    }
+                    "LARGE - 60px" ->{
+                        fontDropDownEditText.setText("60")
+                    }
+                    "LARGER - 75px" ->{
+                        fontDropDownEditText.setText("75")
+                    }
+                    "CUSTOM"->{
+                        fontDropDownEditText.isEnabled = true
+                        fontDropDownEditText.requestFocus()
+                    }
+                }
+            }
+            fontListPopupWindow.dismiss()
+        }
         cancel.setOnClickListener {
             dismiss()
             listener.onCancelled()
+        }
+
+        fontPopupButton.setOnClickListener {
+            fontListPopupWindow.show()
         }
         save.setOnClickListener {
             dismiss()
             listener.onCompleted(editTextFileName.text.toString())
         }
+
         this.setContentView(view)
     }
 
